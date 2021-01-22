e649cf0012230044f262ce0686d4df89daf26f87
Denislav Videnov <denislavv@helecloud.com>
2021-01-22 21:35:02 +0200

    Adding more exercises

---
+++ src/ThirdExercises/SumTheDigits_02.java
@@ -0,0 +1,19 @@
+package ThirdExercises;
+
+import java.util.Scanner;
+
+public class SumTheDigits_02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = Integer.parseInt(scanner.nextLine());
+
+        int sum = 0;
+        while (number != 0) {
+            int lastNumber = number % 10;
+            sum += lastNumber;
+            number = number / 10;
+        }
+
+        System.out.println(sum);
+    }
+}
---
+++ src/ThirdExercises/TriplesOfLatinLatters_06.java
@@ -0,0 +1,19 @@
+package ThirdExercises;
+
+import java.util.Scanner;
+
+public class TriplesOfLatinLatters_06 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        for (char symbol1 = 'a'; symbol1 < 'a' + n; symbol1++) {
+            for (char symbol2 = 'a'; symbol2 < 'a' + n; symbol2++) {
+                for (char symbol3 = 'a'; symbol3 < 'a' + n; symbol3++) {
+                    System.out.printf("%c%c%c", symbol1, symbol2, symbol3);
+                    System.out.println();
+                }
+            }
+        }
+    }
+}
