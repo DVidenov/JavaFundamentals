15063f86f833c0c3ba716387ae6c89e6a64bebf1
Denislav Videnov <ddvidenov@gmail.com>
2021-02-24 21:11:00 +0200

    adding mid exam preparation exercieses

---
+++ src/MidExamPreparation/BlackFlag_01.java
@@ -0,0 +1,31 @@
+package MidExamPreparation;
+
+import java.util.Scanner;
+
+public class BlackFlag_01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int days = Integer.parseInt(scanner.nextLine());
+        int dailyPlunder = Integer.parseInt(scanner.nextLine());
+        int expectedPlunder = Integer.parseInt(scanner.nextLine());
+
+        double totalPlunder = 0;
+        for (int i = 1; i <= days; i++) {
+            totalPlunder += dailyPlunder;
+
+            if (i % 3 == 0){
+                totalPlunder += dailyPlunder * 0.5;
+            }
+            if (i % 5 == 0){
+                totalPlunder = 0.7 * totalPlunder;
+            }
+        }
+        if (totalPlunder >= expectedPlunder){
+            System.out.printf("Ahoy! %.2f plunder gained.", totalPlunder);
+        } else if (totalPlunder < expectedPlunder){
+            double percentOfExpectedPlunder = 0;
+            percentOfExpectedPlunder = (totalPlunder / expectedPlunder) * 100;
+            System.out.printf("Collected only %.2f%% of the plunder.",percentOfExpectedPlunder);
+        }
+    }
+}
---
+++ src/MidExamPreparation/ManOWar_03.java
@@ -0,0 +1,10 @@
+package MidExamPreparation;
+
+import java.util.Scanner;
+
+public class ManOWar_03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
