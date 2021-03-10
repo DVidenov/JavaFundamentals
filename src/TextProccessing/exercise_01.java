f03696da1974f1ad9c4219b70c765a56829ef7de
Denislav Videnov <ddvidenov@gmail.com>
2021-03-10 19:59:21 +0200

    fixing branch

--- src/TextProccessing/exercise_01.java
+++ src/TextProccessing/exercise_01.java
@@ -1,4 +1,26 @@
 package TextProccessing;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class exercise_01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] words = scanner.nextLine().split(" ");
+
+
+        for (String word : words) {
+            String repeatedWord = repeatWord(word, word.length());
+            System.out.println(repeatedWord);
+        }
+    }
+
+    private static String repeatWord(String word, int numberOfRepetitions) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < numberOfRepetitions; i++) {
+            result.append(word);
+        }
+        return result.toString();
+    }
 }
