d64a48eb68eebf93ad46898689fc8ba2630f76c1
Denislav Videnov <ddvidenov@gmail.com>
2021-03-10 19:53:57 +0200

    adding textproccesing exercise

---
+++ src/TextProccessing/RemoveSubstring.java
@@ -0,0 +1,23 @@
+package TextProccessing;
+
+import java.util.Scanner;
+import java.util.concurrent.LinkedBlockingDeque;
+
+public class RemoveSubstring {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        String wordToRemove = scanner.nextLine();
+
+        while (input.contains(wordToRemove)) {
+            input = removeOccurrence(input, wordToRemove);
+        }
+    }
+
+    private static String removeOccurrence(String input, String wordToRemove) {
+        int beginIndex = input.indexOf(wordToRemove);
+        int endIndex = beginIndex + wordToRemove.length();
+        return input.substring(0, beginIndex) + input.substring(endIndex);
+    }
+}
+
