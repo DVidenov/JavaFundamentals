b4b007f46f6f98236315ab9a5035e1cbb5f57bd3
Videnov Denislav (IOC/OPS4) <vid1sf@bosch.com>
2021-05-26 16:19:22 +0300

    dasdas

---
+++ JavaAdvanced/src/StacksAndQueues/ReverseNumbers_01.java
@@ -0,0 +1,23 @@
+package StacksAndQueues;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class ReverseNumbers_01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        //"1 2 3 4 5"
+        String input = scanner.nextLine();
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        String[] splittedInput = input.split(" "); // ["1", "2", "3", "4", "5"]
+        for (String element : splittedInput) {
+            stack.push(Integer.parseInt(element));
+        }
+
+        //stop: stack e empty
+        while(!stack.isEmpty()) {
+            System.out.print(stack.pop() + " ");
+        }
+    }
+}
